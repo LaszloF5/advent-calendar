77be6861c7c6014b2e1f6d06e56672a10ba8187e
LaszloF5 <fazekaslaszlo31@gmail.com>
2024-11-10 14:39:13 +0100

    House build attempt 1.

--- src/App.js
+++ src/App.js
@@ -12,31 +12,32 @@ export default function App() {
   const currDay = Number(new Date().toISOString().split("T")[0].slice(-2));
 
   const story = [
-    ['1. nap. \nAz angyal hírül adja Zakariásnak János születését. Az angyal, Gábriel, megjelenik Zakariásnak a templomban, és közli vele, hogy felesége, Erzsébet, fiút fog szülni, akit Jánosnak kell nevezniük. Ez a gyermek az Úr útját fogja készíteni'],
-    ['2. nap. \nAngyali híradás Máriának. Gábriel megjelenik Máriának, és elmondja neki, hogy fiút fog szülni, akit Jézusnak neveznek majd, és hogy Isten Fia lesz. Mária alázattal elfogadja az angyali hírt.'],
-    ['3. nap. \nMária látogatása Erzsébetnél. Mária meglátogatja Erzsébetet, és amikor Erzsébet meghallja Mária köszöntését, a méhében lévő gyermek megmozdul, Erzsébet pedig áldást mond Máriára.'],
-    ['4. nap. \nJózsef álma. József elhatározza, hogy titokban elbocsátja Máriát, miután megtudja, hogy gyermeket vár. Azonban egy angyal megjelenik neki álmában, és közli vele, hogy a gyermek a Szentlélektől fogant, így József végül magához veszi Máriát.'],
-    ['5. nap. \nA népszámlálás meghirdetése. Caesar Augustus népszámlálást hirdet, így mindenki visszatér szülővárosába. József és Mária Betlehembe indulnak, mivel József Dávid házából származik.'],
-    ['6. nap. \nJózsef és Mária megérkeznek Betlehembe. Hosszú út után Betlehembe érnek, ahol nincs számukra hely a szálláson, így egy egyszerű istállóban keresnek menedéket.'],
-    ['7. nap. \nJézus megszületik. Az éjszaka folyamán Mária világra hozza Jézust. Az újszülöttet bepólyálja, és egy jászolba helyezi, mivel nem találnak helyet a fogadóban.'],
-    ['8. nap. \nAngyalok jelennek meg a pásztoroknak. Kinn a mezőn pásztorok őrzik a nyájat, amikor megjelenik nekik egy angyal, és elmondja a jó hírt: "Ma megszületett a Megváltó, aki az Úr Krisztus."'],
-    ['9. nap. \nAz angyalok éneke. Hirtelen egy mennyei sereg jelenik meg az angyallal, és dicséretet zengenek: "Dicsőség a magasságban Istennek, és békesség a földön az embereknek."'],
-    ['10. nap. \nA pásztorok útnak indulnak. A pásztorok Betlehembe sietnek, hogy lássák a gyermek Jézust. Rátalálnak Máriára, Józsefre és a jászolban fekvő gyermekre.'],
-    ['11. nap. \nA pásztorok hírül adják a történteket. Miután találkoztak Jézussal, a pásztorok mindenfelé elmondják, amit láttak és hallottak. Az emberek csodálkozva hallgatják őket.'],
-    ['12. nap. \nMária szívében megőrzi a történteket. Mária elmélyedve, szívében megőrzi és fontolgatja mindazt, amit látott és hallott az angyalokról és a pásztorokról.'],
-    ['13. nap. \nJézus névadása. Nyolc nap elteltével elnevezik a gyermeket Jézusnak, ahogyan az angyal meghagyta Máriának.'],
-    ['14. nap. \nA bölcsek elindulnak kelet felől. Keleten bölcsek látják meg a különleges csillagot, amely egy király születését jelzi, és útnak indulnak, hogy köszöntsék őt.'],
-    ['15. nap. \nA bölcsek Heródesnél érdeklődnek. Jeruzsálembe érve a bölcsek megkérdezik Heródest, hogy hol találják a zsidók újszülött királyát, mert látni akarják őt.'],
-    ['16. nap. \nHeródes tanácsot kér a papoktól. Heródes megdöbben a hír hallatán, és összehívja a papokat és írástudókat, hogy megtudja, hol kell születnie a Messiásnak.'],
-    ['17. nap. \nA próféta jövendölése Betlehemről. A papok elmondják, hogy a próféciák szerint a Messiás Betlehemben születik, mivel az Dávid városa.'],
-    ['18. nap. \nHeródes megbízza a bölcseket. Heródes azt kéri a bölcsektől, hogy miután megtalálták a gyermeket, térjenek vissza hozzá, hogy ő is hódolhasson előtte.'],
-    ['19. nap. \nA bölcsek követik a csillagot. A bölcsek Betlehembe mennek, és örömmel veszik észre, hogy a csillag megáll egy ház felett, ahol a gyermek Jézus van.'],
-    ['20. nap. \nA bölcsek hódolata és ajándékai. A bölcsek belépnek, és hódolnak Jézus előtt, majd ajándékokat adnak: aranyat, tömjént és mirhát.'],
-    ['21. nap. \nFigyelmeztetés álomban. Álomban figyelmeztetést kapnak, hogy ne térjenek vissza Heródeshez, így más úton térnek haza.'],
-    ['22. nap. \nAz Úr figyelmezteti Józsefet. Egy angyal álmában figyelmezteti Józsefet, hogy meneküljön Egyiptomba, mert Heródes a gyermek életére tör.'],
-    ['23. nap. \nA menekülés Egyiptomba. József azonnal útra kel Máriával és Jézussal, és Egyiptomba menekülnek, hogy biztonságban legyenek.'],
-    ['24. nap. \nJézus születésének örömhíre. A hírek terjednek Jézus születéséről, aki később a világ Megváltója lesz. Az emberek reménnyel és örömmel várják az ígéretes jövőt.'],
+    ['1. nap \nAngyali híradás Zakariásnak \nAz angyal, Gábriel, megjelenik Zakariásnak a templomban, és közli vele, hogy felesége, Erzsébet, fiút fog szülni, akit Jánosnak kell nevezniük. Ez a gyermek az Úr útját fogja készíteni'],
+    ['2. nap \nAngyali híradás Máriának \nGábriel megjelenik Máriának, és elmondja neki, hogy fiút fog szülni, akit Jézusnak neveznek majd, és hogy Isten Fia lesz. Mária alázattal elfogadja az angyali hírt.'],
+    ['3. nap \nMária látogatása Erzsébetnél \nMária meglátogatja Erzsébetet, és amikor Erzsébet meghallja Mária köszöntését, a méhében lévő gyermek megmozdul, Erzsébet pedig áldást mond Máriára.'],
+    ['4. nap \nJózsef álma \nJózsef elhatározza, hogy titokban elbocsátja Máriát, miután megtudja, hogy gyermeket vár. Azonban egy angyal megjelenik neki álmában, és közli vele, hogy a gyermek a Szentlélektől fogant, így József végül magához veszi Máriát.'],
+    ['5. nap \nA népszámlálás meghirdetése \nCaesar Augustus népszámlálást hirdet, így mindenki visszatér szülővárosába. József és Mária Betlehembe indulnak, mivel József Dávid házából származik.'],
+    ['6. nap \nJózsef és Mária megérkeznek Betlehembe \nHosszú út után Betlehembe érnek, ahol nincs számukra hely a szálláson, így egy egyszerű istállóban keresnek menedéket.'],
+    ['7. nap \nJézus megszületik \nAz éjszaka folyamán Mária világra hozza Jézust. Az újszülöttet bepólyálja, és egy jászolba helyezi, mivel nem találnak helyet a fogadóban.'],
+    ['8. nap \nAngyalok jelennek meg a pásztoroknak \nKinn a mezőn pásztorok őrzik a nyájat, amikor megjelenik nekik egy angyal, és elmondja a jó hírt: "Ma megszületett a Megváltó, aki az Úr Krisztus."'],
+    ['9. nap \nAz angyalok éneke \nHirtelen egy mennyei sereg jelenik meg az angyallal, és dicséretet zengenek: "Dicsőség a magasságban Istennek, és békesség a földön az embereknek."'],
+    ['10. nap \nA pásztorok útnak indulnak \nA pásztorok Betlehembe sietnek, hogy lássák a gyermek Jézust. Rátalálnak Máriára, Józsefre és a jászolban fekvő gyermekre.'],
+    ['11. nap \nA pásztorok hírül adják a történteket', 'Miután találkoztak Jézussal, a pásztorok mindenfelé elmondják, amit láttak és hallottak. Az emberek csodálkozva hallgatják őket.'],
+    ['12. nap \nMária szívében megőrzi a történteket \nMária elmélyedve, szívében megőrzi és fontolgatja mindazt, amit látott és hallott az angyalokról és a pásztorokról.'],
+    ['13. nap \nJézus névadása \nNyolc nap elteltével elnevezik a gyermeket Jézusnak, ahogyan az angyal meghagyta Máriának.'],
+    ['14. nap \nA bölcsek elindulnak kelet felől \nKeleten bölcsek látják meg a különleges csillagot, amely egy király születését jelzi, és útnak indulnak, hogy köszöntsék őt.'],
+    ['15. nap \nA bölcsek Heródesnél érdeklődnek \nJeruzsálembe érve a bölcsek megkérdezik Heródest, hogy hol találják a zsidók újszülött királyát, mert látni akarják őt.'],
+    ['16. nap \nHeródes tanácsot kér a papoktól \nHeródes megdöbben a hír hallatán, és összehívja a papokat és írástudókat, hogy megtudja, hol kell születnie a Messiásnak.'],
+    ['17. nap \nA próféta jövendölése Betlehemről \nA papok elmondják, hogy a próféciák szerint a Messiás Betlehemben születik, mivel az Dávid városa.'],
+    ['18. nap \nHeródes megbízza a bölcseket \nHeródes azt kéri a bölcsektől, hogy miután megtalálták a gyermeket, térjenek vissza hozzá, hogy ő is hódolhasson előtte.'],
+    ['19. nap \nA bölcsek követik a csillagot \nA bölcsek Betlehembe mennek, és örömmel veszik észre, hogy a csillag megáll egy ház felett, ahol a gyermek Jézus van.'],
+    ['20. nap \nA bölcsek hódolata és ajándékai \nA bölcsek belépnek, és hódolnak Jézus előtt, majd ajándékokat adnak: aranyat, tömjént és mirhát.'],
+    ['21. nap \nFigyelmeztetés álomban \nÁlomban figyelmeztetést kapnak, hogy ne térjenek vissza Heródeshez, így más úton térnek haza.'],
+    ['22. nap \nAz Úr figyelmezteti Józsefet \nEgy angyal álmában figyelmezteti Józsefet, hogy meneküljön Egyiptomba, mert Heródes a gyermek életére tör.'],
+    ['23. nap \nA menekülés Egyiptomba \nJózsef azonnal útra kel Máriával és Jézussal, és Egyiptomba menekülnek, hogy biztonságban legyenek.'],
+    [`24. nap \nJézus születésének örömhíre \nA hírek terjednek Jézus születéséről, aki később a világ Megváltója lesz. Az emberek reménnyel és örömmel várják az ígéretes jövőt. \nÁldott, békés, boldog karácsonyt ${name}`],
   ];
+  
 
   const handleName = (e) => {
     e.preventDefault();
@@ -74,6 +75,7 @@ export default function App() {
       setName(yourName);
       setNeedName(false);
     } else {
+      // alert('Add meg a neved!');
       setNeedName(true);
     }
   }, []);
@@ -84,9 +86,13 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Advent calendar {name}</h1>
+      <h1 className="h1">Advent calendar</h1>
       <button onClick={handleTestName}>Delete all names</button>
-      {needName && (<form onSubmit={handleName} className="form-name">
+      {needName ? (<div className="house">
+        <div className="roof">
+          <span className="leftSide-roof"></span>
+        </div>
+        <form onSubmit={handleName} className="form-name">
         <label className="form-name_label" htmlFor='nameId'>
           Name:
           </label>
@@ -99,8 +105,11 @@ export default function App() {
             required 
             placeholder="ex.: James" />
         <button type="submit" className="form-name_btn">Submit</button>
-      </form>)}
-      <div className="container">
+      </form>
+      <div className="window"></div>
+      <div className="door"><span className="doorknob"></span></div>
+      </div>) :
+      (<div className="container">
         {item.map((value, index) => (
           <Card
             key={index}
@@ -113,7 +122,7 @@ export default function App() {
             onClick={() => handleClick(index, index + 1)}
           />
         ))}
-      </div>
+      </div>)}
     </div>
   );
 }
