2ea374edc489db77f59c3c42d79172ab6fb399d1
LaszloF5 <fazekaslaszlo31@gmail.com>
2024-11-18 15:53:27 +0100

    Numbers modified.

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import './Card.css';
 
 export default function Card({ value, index, isBack, onClick, date, name, story, imgClass }) {
-
     return (
-        <div style={{"--content-text": `"${date}"`}} className={`card ${isBack ? 'flip' : ''} ${imgClass}`} onClick={onClick}>
+        <div style={{"--content-text": `"${date}."` }} className={`card ${isBack ? 'flip' : ''} ${imgClass}`} onClick={onClick}>
     <div className="card-content">
         {story[index][0].split('\n').map((line, i) => (<p key={i}>{line}</p>))}
     </div>
